1958ab9d3e3dfa41a6ec5b5d06eae5b591c92069
kleemeo <leenexer@gmail.com>
2023-02-28 14:31:01 -0500

    global types

---
+++ app/global.d.ts
@@ -0,0 +1,11 @@
+declare interface IFormData {
+  item: string;
+  date: string;
+  'category[Category]': string;
+  'category[Code]': string;
+  'category[Group]': string;
+  'category[Necessary]': string;
+  'account[Account]': string;
+  'account[Type]': string;
+  amount: string;
+}
